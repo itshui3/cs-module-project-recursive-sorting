b4dda482d822681a6ece090ca9eb107fd2e0e122
Hui <jeffrey.x.hui@gmail.com>
2020-07-04 20:19:56 -0700

    completed quickSort pivoting in the middle

--- src/sorting/test_sorting.py
+++ src/sorting/test_sorting.py
@@ -3,6 +3,7 @@ import random
 from sorting import *
 from quickSort import *
 from quickSort_lastPivot import *
+from quickSort_midPivot import *
 
 class RecursiveSortingTests(unittest.TestCase):
     def test_merge_sort(self):
@@ -43,6 +44,19 @@ class RecursiveSortingTests(unittest.TestCase):
         self.assertEqual(quickSort_last(arr3), [2])
         self.assertEqual(quickSort_last(arr4), [0, 1, 2, 3, 4, 5])
         self.assertEqual(quickSort_last(arr5), sorted(arr5))
+
+    def test_quick_sort_mid(self):
+        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+        arr2 = []
+        arr3 = [2]
+        arr4 = [0, 1, 2, 3, 4, 5]
+        arr5 = random.sample(range(200), 50)
+
+        self.assertEqual(quickSort_mid(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.assertEqual(quickSort_mid(arr2), [])
+        self.assertEqual(quickSort_mid(arr3), [2])
+        self.assertEqual(quickSort_mid(arr4), [0, 1, 2, 3, 4, 5])
+        self.assertEqual(quickSort_mid(arr5), sorted(arr5))
 
     # Uncomment this test to test your in-place merge sort implementation
     # def test_in_place_merge_sort(self):
